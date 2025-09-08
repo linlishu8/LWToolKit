438abcd7a492a0f7c32f22b503a5cefaa77414f4
Andy <linlishu8@163.com>
2025-09-09 01:07:29 +0800

    喜欢重名文件

--- Sources/LWNetwork/Reachability/LWReachability.swift
+++
@@ -1,3 +0,0 @@
-import Foundation
-import Network
-public final class LWReachability { public static let shared = LWReachability(); private let monitor = NWPathMonitor(); private let q = DispatchQueue(label: "lw.reach"); public private(set) var isReachable = true; public func start(){ monitor.pathUpdateHandler = { [weak self] p in self?.isReachable = (p.status == .satisfied) }; monitor.start(queue: q) } ; public func stop(){ monitor.cancel() } }
